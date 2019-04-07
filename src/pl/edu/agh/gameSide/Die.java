00c0a432531e0693926f957321c93fcdec550163
Alexis Mathoux <alexis.mathoux@gmail.com>
2019-04-07 16:51:23 +0200

    Restructuration

---
+++ src/pl/edu/agh/gameSide/Die.java
@@ -0,0 +1,28 @@
+package pl.edu.agh.gameSide;
+
+import java.util.Random;
+
+public class Die
+{
+
+    private int value;
+
+    public Die()
+    {
+        this.value = 0;
+    }
+
+    public int rollTheDie()
+    {
+        Random rng = new Random();
+        this.value = rng.nextInt(6) + 1;
+        return value;
+
+    }
+
+    public int getValue()
+    {
+        return this.value;
+    }
+
+}
